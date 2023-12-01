7cea5a5ab49207f044aeb559706add69937f1753
ShadiSoufan44 <shadi_soufan@brown.edu>
2023-11-30 21:40:49 -0500

    Feat/Finished Calendar and created Add/Remove meals methods.

--- frontend/repl/src/components/Display.tsx
+++ frontend/repl/src/components/Display.tsx
@@ -2,22 +2,40 @@ import { useState } from "react";
 import "../styles/main.css";
 import { History } from "./History";
 import { Input } from "./Input";
+import Calendar from "./Calendar"; // Import the Calendar component
 import React from "react";
 
 /**
  * The main component.
  *
- * This component represents a simple interface that displays a history of commands and their results. It consists of an input section where users can enter commands and a history section that displays the executed commands and their outcomes.
+ * This component represents a simple interface that displays a history of commands and their results.
+ * It consists of an input section where users can enter commands, a calendar for displaying a weekly meal plan,
+ * and a history section that displays the executed commands and their outcomes.
  *
  * @returns - A React JSX element representing the REPL component.
  */
 export default function Display() {
   const [history, setHistory] = useState<string[]>([]);
+  const [mealPlan, setMealPlan] = useState<string[]>([]);
+
+  // Function to add a meal to the meal plan
+  const addMeal = (meal: string) => {
+    setMealPlan([...mealPlan, meal]);
+  };
+
+  // Function to remove a meal from the meal plan
+  const removeMeal = (index: number) => {
+    const updatedMealPlan = [...mealPlan];
+    updatedMealPlan.splice(index, 1);
+    setMealPlan(updatedMealPlan);
+  };
+
   return (
     <div className="Display">
       {/* <History history={history} />
       <hr></hr> */}
       <Input history={history} setHistory={setHistory} />
+      <Calendar mealPlan={mealPlan} removeMeal={removeMeal} />
     </div>
   );
 }

b6c69ee38d5595ba8f00af8b99021e8957cf63b8
annie-ye4 <113400571+annie-ye4@users.noreply.github.com>
2023-12-10 18:43:29 -0500

    feat: there are at least two items in every category

--- frontend/repl/src/components/Selector.tsx
+++ frontend/repl/src/components/Selector.tsx
@@ -67,18 +67,22 @@ export function processSearchResult(commandString: string, searchData: string | 
       // Function to check if an item is already selected
       const isItemSelected = (item: string) => selectedItems.some(selected => selected.item === item);
 
-      // Select one item from each meal
+      // Select two items from each meal
       ['breakfast', 'lunch', 'dinner'].forEach(meal => {
         const mealArray = meal === 'breakfast' ? breakfastArray : meal === 'lunch' ? lunchArray : dinnerArray;
         if (mealArray.length > 0) {
-          const item = mealArray[0];
-          selectedItems.push({
-            meal,
-            item: item.item,
-            calories: parseInt(item.calories),
-            portionSize: item.portionSize,
-          });
-          totalCalories += parseInt(item.calories);
+          for (const item of mealArray.slice(0, 2)) {
+            // Check if the item is not already selected and adding it won't exceed total calories
+            if (!isItemSelected(item.item) && totalCalories + parseInt(item.calories) <= calories) {
+              selectedItems.push({
+                meal,
+                item: item.item,
+                calories: parseInt(item.calories),
+                portionSize: item.portionSize,
+              });
+              totalCalories += parseInt(item.calories);
+            }
+          }
         }
       });
 
@@ -91,7 +95,7 @@ export function processSearchResult(commandString: string, searchData: string | 
 
         // Loop through items in the mealArray
         let remainingItemsToAdd = remainingTargetItemCount;
-        for (const item of mealArray.slice(1)) {
+        for (const item of mealArray.slice(2)) {
           // Check if the item is not already selected and adding it won't exceed total calories
           if (!isItemSelected(item.item) && totalCalories + parseInt(item.calories) <= calories) {
             selectedItems.push({

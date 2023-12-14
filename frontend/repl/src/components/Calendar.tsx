defdc136d845c50f8403a94ec615a59442d2bb45
annie-ye4 <113400571+annie-ye4@users.noreply.github.com>
2023-12-14 15:24:40 -0500

    feat: working on calendar

--- frontend/repl/src/components/Calendar.tsx
+++ frontend/repl/src/components/Calendar.tsx
@@ -5,62 +5,98 @@ interface CalendarProps {
   removeMeal: (index: number) => void;
 }
 
-const Calendar: React.FC<CalendarProps> = ({ mealPlan, removeMeal }) => {
-  const daysOfWeek = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+// const Calendar: React.FC<CalendarProps> = ({ mealPlan, removeMeal }) => {
+//   const daysOfWeek = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
 
+//   return (
+//     <div className="calendar">
+//       <table>
+//         <thead>
+//           <tr>
+//             <th>Time</th>
+//             {daysOfWeek.map((day) => (
+//               <th key={day}>{day}</th>
+//             ))}
+//           </tr>
+//         </thead>
+//         <tbody>
+//           <tr>
+//             <td id="col1">Breakfast</td>
+//             {daysOfWeek.map((day, index) => (
+//               <td key={index}>
+//                 <MealSlot
+//                   day={day}
+//                   mealPlan={mealPlan}
+//                   removeMeal={removeMeal}
+//                 />
+//               </td>
+//             ))}
+//           </tr>
+//           <tr>
+//             <td id="col2">Lunch</td>
+//             {daysOfWeek.map((day, index) => (
+//               <td key={index + daysOfWeek.length}>
+//                 <MealSlot
+//                   day={day}
+//                   mealPlan={mealPlan}
+//                   removeMeal={removeMeal}
+//                 />
+//               </td>
+//             ))}
+//           </tr>
+//           <tr>
+//             <td id="col3">Dinner</td>
+//             {daysOfWeek.map((day, index) => (
+//               <td key={index + 2 * daysOfWeek.length}>
+//                 <MealSlot
+//                   day={day}
+//                   mealPlan={mealPlan}
+//                   removeMeal={removeMeal}
+//                 />
+//               </td>
+//             ))}
+//           </tr>
+//         </tbody>
+//       </table>
+//     </div>
+//   );
+// };
+
+const CalendarTable: React.FC<CalendarProps> = ({ mealPlan, removeMeal }) => {
   return (
-    <div className="calendar">
+    <div className="meal-table">
       <table>
         <thead>
           <tr>
             <th>Time</th>
-            {daysOfWeek.map((day) => (
-              <th key={day}>{day}</th>
-            ))}
+            <th>Meal</th>
           </tr>
         </thead>
         <tbody>
           <tr>
             <td id="col1">Breakfast</td>
-            {daysOfWeek.map((day, index) => (
-              <td key={index}>
-                <MealSlot
-                  day={day}
-                  mealPlan={mealPlan}
-                  removeMeal={removeMeal}
-                />
-              </td>
-            ))}
+            <td>
+              <MealSlot day="Breakfast" mealPlan={mealPlan} removeMeal={removeMeal} />
+            </td>
           </tr>
           <tr>
             <td id="col2">Lunch</td>
-            {daysOfWeek.map((day, index) => (
-              <td key={index + daysOfWeek.length}>
-                <MealSlot
-                  day={day}
-                  mealPlan={mealPlan}
-                  removeMeal={removeMeal}
-                />
-              </td>
-            ))}
+            <td>
+              <MealSlot day="Lunch" mealPlan={mealPlan} removeMeal={removeMeal} />
+            </td>
           </tr>
           <tr>
             <td id="col3">Dinner</td>
-            {daysOfWeek.map((day, index) => (
-              <td key={index + 2 * daysOfWeek.length}>
-                <MealSlot
-                  day={day}
-                  mealPlan={mealPlan}
-                  removeMeal={removeMeal}
-                />
-              </td>
-            ))}
+            <td>
+              <MealSlot day="Dinner" mealPlan={mealPlan} removeMeal={removeMeal} />
+            </td>
           </tr>
         </tbody>
       </table>
     </div>
   );
 };
+
 
 interface MealSlotProps {
   day: string;

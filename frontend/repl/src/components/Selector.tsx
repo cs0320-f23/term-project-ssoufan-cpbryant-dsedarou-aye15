1c93090a260d93f485373981d4c77745dac06576
danielsedarous <daniel_sedarous@brown.edu>
2023-12-14 16:26:34 -0500

    feat: changed calendar to a singular day

--- frontend/repl/src/components/Selector.tsx
+++ frontend/repl/src/components/Selector.tsx
@@ -9,12 +9,14 @@ export interface SelectorFunction {
   (args: Array<string>): Promise<string>;
 }
 
-
-export function searchCommandFunction(selectedOption: string, commandString: string): Promise<string> {
+export function searchCommandFunction(
+  selectedOption: string,
+  commandString: string
+): Promise<string> {
   return new Promise(async (resolve) => {
     try {
       const response = await fetch(
-        `http://localhost:2023/menu?restriction=` + selectedOption
+        `http://localhost:2025/menu?restriction=` + selectedOption
       );
       const json = await response.json();
       console.log(json);
@@ -34,46 +36,82 @@ export function searchCommandFunction(selectedOption: string, commandString: str
   });
 }
 
-export function processSearchResult(commandString: string, searchData: string | object): void {
+export function processSearchResult(
+  commandString: string,
+  searchData: string | object
+): void {
   try {
     const calories = parseInt(commandString);
     console.log("Command String:", calories);
 
     // Check if the searchData is already an object
-    const parsedData = typeof searchData === 'string' ? JSON.parse(searchData) : searchData;
+    const parsedData =
+      typeof searchData === "string" ? JSON.parse(searchData) : searchData;
 
     // Check if the parsedData is an object
-    if (typeof parsedData === 'object' && !Array.isArray(parsedData)) {
-      const breakfastArray: { item: string, calories: string, portionSize: string }[] = [];
-      const lunchArray: { item: string, calories: string, portionSize: string }[] = [];
-      const dinnerArray: { item: string, calories: string, portionSize: string }[] = [];
+    if (typeof parsedData === "object" && !Array.isArray(parsedData)) {
+      const breakfastArray: {
+        item: string;
+        calories: string;
+        portionSize: string;
+      }[] = [];
+      const lunchArray: {
+        item: string;
+        calories: string;
+        portionSize: string;
+      }[] = [];
+      const dinnerArray: {
+        item: string;
+        calories: string;
+        portionSize: string;
+      }[] = [];
 
       Object.keys(parsedData).forEach((key) => {
         const item = parsedData[key];
-        const itemInfo = { item: item.Item, calories: item.Calories, portionSize: item['Serving size'] };
+        console.log("meals: " + item.Meal)
+        const itemInfo = {
+          item: item.Item,
+          calories: item.Calories,
+          portionSize: item["Serving size"],
+        };
 
-        if (item.Meal.toLowerCase() === 'breakfast') {
+        if (item.Meal.toLowerCase() === "breakfast") {
           breakfastArray.push(itemInfo);
-        } else if (item.Meal.toLowerCase() === 'lunch') {
+          console.log("breakfast: " + breakfastArray)
+        } else if (item.Meal.toLowerCase() === "lunch") {
           lunchArray.push(itemInfo);
-        } else if (item.Meal.toLowerCase() === 'dinner') {
+        } else if (item.Meal.toLowerCase() === "dinner") {
           dinnerArray.push(itemInfo);
         }
       });
 
-      const selectedItems: { meal: string, item: string, calories: number, portionSize: string }[] = [];
+      const selectedItems: {
+        meal: string;
+        item: string;
+        calories: number;
+        portionSize: string;
+      }[] = [];
       let totalCalories = 0;
 
       // Function to check if an item is already selected
-      const isItemSelected = (item: string) => selectedItems.some(selected => selected.item === item);
+      const isItemSelected = (item: string) =>
+        selectedItems.some((selected) => selected.item === item);
 
       // Select two items from each meal
-      ['breakfast', 'lunch', 'dinner'].forEach(meal => {
-        const mealArray = meal === 'breakfast' ? breakfastArray : meal === 'lunch' ? lunchArray : dinnerArray;
+      ["breakfast", "lunch", "dinner"].forEach((meal) => {
+        const mealArray =
+          meal === "breakfast"
+            ? breakfastArray
+            : meal === "lunch"
+            ? lunchArray
+            : dinnerArray;
         if (mealArray.length > 0) {
           for (const item of mealArray.slice(0, 2)) {
             // Check if the item is not already selected and adding it won't exceed total calories
-            if (!isItemSelected(item.item) && totalCalories + parseInt(item.calories) <= calories) {
+            if (
+              !isItemSelected(item.item) &&
+              totalCalories + parseInt(item.calories) <= calories
+            ) {
               selectedItems.push({
                 meal,
                 item: item.item,
@@ -87,17 +125,27 @@ export function processSearchResult(commandString: string, searchData: string | 
       });
 
       // Calculate the remaining target number of items for each meal
-      const remainingTargetItemCount = Math.floor((calories - totalCalories) / 2);
+      const remainingTargetItemCount = Math.floor(
+        (calories - totalCalories) / 2
+      );
 
       // Loop through meals in order: breakfast, lunch, dinner
-      ['breakfast', 'lunch', 'dinner'].forEach(meal => {
-        const mealArray = meal === 'breakfast' ? breakfastArray : meal === 'lunch' ? lunchArray : dinnerArray;
+      ["breakfast", "lunch", "dinner"].forEach((meal) => {
+        const mealArray =
+          meal === "breakfast"
+            ? breakfastArray
+            : meal === "lunch"
+            ? lunchArray
+            : dinnerArray;
 
         // Loop through items in the mealArray
         let remainingItemsToAdd = remainingTargetItemCount;
         for (const item of mealArray.slice(2)) {
           // Check if the item is not already selected and adding it won't exceed total calories
-          if (!isItemSelected(item.item) && totalCalories + parseInt(item.calories) <= calories) {
+          if (
+            !isItemSelected(item.item) &&
+            totalCalories + parseInt(item.calories) <= calories
+          ) {
             selectedItems.push({
               meal,
               item: item.item,
@@ -113,11 +161,14 @@ export function processSearchResult(commandString: string, searchData: string | 
           }
         }
       });
+      const selectedItemsForBreakfast = selectedItems.filter(
+        (item) => item.meal === "Breakfast"
+      );
 
       // Log the selected items
       console.log("Selected Items:", selectedItems);
     } else {
-      console.error('Parsed data is not an object.');
+      console.error("Parsed data is not an object.");
     }
   } catch (error) {
     console.error("Error processing search result:", error);
@@ -135,10 +186,12 @@ export function Selector(props: SelectorProps) {
   const handleOptionChange = async (selectedValue: string) => {
     setSelectedOption(selectedValue);
     props.onSelect(selectedValue);
-    const result = await searchCommandFunction(selectedValue, props.commandString);
+    const result = await searchCommandFunction(
+      selectedValue,
+      props.commandString
+    );
     // Use the result as needed
   };
-
 
   return (
     <div>

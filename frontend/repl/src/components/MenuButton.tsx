ec3eea421df0eee08811acfe07ee2af04ee7b609
ShadiSoufan44 <shadi_soufan@brown.edu>
2023-12-18 13:11:25 -0500

    Feat/Finished designing table for popup. TODO/Fill inrest of table

--- frontend/repl/src/components/MenuButton.tsx
+++ frontend/repl/src/components/MenuButton.tsx
@@ -11,20 +11,7 @@ export function MenuButton() {
       <button id="MenuButton" onClick={() => setButtonPopup(true)}>
         Click Here to View the Menu!
       </button>
-      <Popup trigger={buttonPopup} setTrigger={setButtonPopup}>
-        <p>
-          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Recusandae
-          eum dolore nulla consequuntur perferendis laudantium ratione iure odit
-          ipsum quae vitae ullam, quis explicabo, consectetur autem, aliquid
-          cupiditate aperiam! Beatae.
-        </p>
-        <p>
-          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Porro,
-          voluptatibus, sunt provident sit odit ipsam nisi accusamus amet aut,
-          fugiat incidunt reiciendis sequi? Quidem nemo reprehenderit ab culpa.
-          Aspernatur, illo!
-        </p>
-      </Popup>
+      <Popup trigger={buttonPopup} setTrigger={setButtonPopup}></Popup>
     </div>
   );
 }
--- frontend/repl/src/components/Popup.tsx
+++ frontend/repl/src/components/Popup.tsx
@@ -6,6 +6,72 @@ function Popup(props) {
   return props.trigger ? (
     <div className="popup">
       <div className="popup-inner">
+        <h2>Specialty Items For Each Day</h2>
+        <table>
+          <tr>
+            <th>Sunday</th>
+            <th>Monday</th>
+            <th>Tuesday</th>
+            <th>Wednesday</th>
+            <th>Thursday</th>
+            <th>Friday</th>
+            <th>Saturday</th>
+          </tr>
+          <tr>
+            <td>Ratty Blueberry French Toast Casserole</td>
+            <td>Ratty Cranberry Coffee Cake</td>
+            <td>VDub Tex Mex Tofu Scramble</td>
+            <td>Vdub Applewood Smoked Bacon</td>
+            <td>Vdub Vegan Paprika Breakfast Potatoes</td>
+            <td>Ratty Lentils W/ Roasted Vegetables</td>
+            <td>Ratty Jelly Donut</td>
+          </tr>
+          <tr>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+          </tr>
+          <tr>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+          </tr>
+          <tr>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+          </tr>
+          <tr>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+          </tr>
+          <tr>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+          </tr>
+        </table>
         <button
           className="close-button"
           onClick={() => props.setTrigger(false)}
